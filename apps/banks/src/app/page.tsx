28ad60ecefed870c68943e56940d2cf5095da83e
ken0063 <olukotunk@gmail.com>
2023-08-02 01:21:34 +0100

    Initial commit

---
+++ apps/banks/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "ui";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      Banks
+      <Header text="Welcome to bank" />
+    </main>
+  );
+}
---
+++ apps/business/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      Business
+      <Button />
+    </main>
+  );
+}
